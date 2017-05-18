145285a2c7a104100cc6c73d6a51fcffcb6eec0c
Logan Rakai <lmrakai@gmail.com>
2017-05-19 00:52:48 +0800

    include content advisor logic

--- src/app.py
+++ src/app.py
@@ -1,6 +1,11 @@
+'''An advisor on what content type to try'''
 from flask import Flask
+from random import randint
 app = Flask(__name__)
+
+content = ['learning path', 'lab', 'course', 'quiz']
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'How about trying a ' + content[randint(0,2)] + ' next'
